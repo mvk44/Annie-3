902ea28055face8eac475e5c882e775e2bee0a87
4nni33 <108834497+4nni33@users.noreply.github.com>
2023-11-04 02:57:40 -0300

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,21 +1,19 @@
-const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
-  if (usedPrefix == 'a' || usedPrefix == 'A') return;
-  if (!(isAdmin || isOwner)) {
-    global.dfail('admin', m, conn);
-    throw false;
-  }
-  const pesan = args.join` `;
-  const oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`;
-  let teks = `*⺀𝗔𝗖𝗧𝗜𝗩𝗔𝗧𝗘 𝗣𝗜𝗡𝗖𝗛𝗘 𝗔𝗗𝗢𝗥𝗡𝗢⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
-  for (const mem of participants) {
-    teks += `┣➥🦥 @${mem.id.split('@')[0]}\n`;
-  }
-  teks += `*└*💖𝐌𝐈𝐂𝐇 𝐁𝐎𝐓💖\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
-  conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
-};
-handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
-handler.tags = ['group'];
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i;
-handler.admin = true;
-handler.group = true;
-export default handler;
+let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}
+let pesan = args.join` `
+let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}\n`
+let teks = `ততত ʚ🩵ɞ  *¡ATENCIÓN!*✨ ⤾·˚ ༘ ততত\n┈┈┈┈┈ ๑ ⋅ ⋯ ୨ ୧ ⋯ ⋅ ๑ ┈┈┈┈┈\n${oi}\n_⋆ ࣪.𓏲 ๋࣭ Personitas:_\n`
+for (let mem of participants) {
+teks += `❄️໋᳝݊▹֛◌  @${mem.id.split('@')[0]}\n`}
+teks += `┈┈┈┈┈ ๑ ⋅ ⋯ ୨ ୧ ⋯ ⋅ ๑ ┈┈┈┈┈\n▒̶᩠߲⃡ࠖ᩼᭭݊⃝⃡🩵ࣱꪾ῾֣ؐ  𝘈𝘯𝘯𝘪𝘦𝘉𝘰𝘵 °୭`
+conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
+}
+handler.help = ['tagall <mesaje>','invocar <mesaje>']
+handler.tags = ['group']
+handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
+handler.admin = true
+handler.group = true
+export default handler

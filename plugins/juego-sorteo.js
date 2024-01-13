2ed7b13a40837aabd66b475c900041e639d09169
4nni33 <108834497+4nni33@users.noreply.github.com>
2024-01-13 02:11:07 -0300

    Update juego-sorteo.js

--- plugins/juego-sorteo.js
+++ plugins/juego-sorteo.js
@@ -1,29 +1,38 @@
 import util from 'util'
 import path from 'path'
 
-async function handler(m, { groupMetadata, command, conn, text, usedPrefix}) {
+async function handler(m, { groupMetadata, command, conn, text, usedPrefix }) {
+  let user = a => '@' + a.split('@')[0]
+  if (!text) throw `*⚠️ EJEMPLO DE USO:*\n${usedPrefix + command} nombre1 nombre2 nombre3`
+  
+  // Obtener una lista de nombres o números proporcionados por el usuario
+  let entries = text.trim().split(' ');
 
-let user = a => '@' + a.split('@')[0]
-if (!text) throw `*⚠️ EJEMPLO DE USO:*\n${usedPrefix + command} texto`
-let ps = groupMetadata.participants.map(v => v.id)
-let a = ps.getRandom()
-let k = Math.floor(Math.random() * 70)
-let vn = `https://hansxd.nasihosting.com/sound/sound${k}.mp3`
-let top = `*😼 ${user(a)} Acaba de ganar el sorteo felicitaciones 🎉*`
-let txt = ''
-let count = 0
-for (const c of top) {
-await new Promise(resolve => setTimeout(resolve, 15))
-txt += c
-count++
+  // Si no se proporciona al menos un nombre o número, mostrar un mensaje de error
+  if (entries.length === 0) throw 'Debes proporcionar al menos un nombre o número para el sorteo.';
 
-if (count % 10 === 0) {
-conn.sendPresenceUpdate('composing' , m.chat);
-}
-}
-await conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100} )
+  let winner = pickRandom(entries); // Elegir un ganador al azar de la lista
+
+  let k = Math.floor(Math.random() * 70)
+  let vn = `https://hansxd.nasihosting.com/sound/sound${k}.mp3`
+  let top = `*😼 ${user(winner)} Acaba de ganar el sorteo felicitaciones 🎉*`
+  let txt = ''
+  let count = 0
+
+  for (const c of top) {
+    await new Promise(resolve => setTimeout(resolve, 15))
+    txt += c
+    count++
+
+    if (count % 10 === 0) {
+      conn.sendPresenceUpdate('composing', m.chat);
+    }
+  }
+
+  await conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, { quoted: m, ephemeralExpiration: 24 * 60 * 100, disappearingMessagesInChat: 24 * 60 * 100 })
 
 }
+
 handler.help = ['sorteo']
 handler.command = ['sorteo']
 handler.tags = ['juegos']
@@ -33,4 +42,5 @@ handler.admin = true
 export default handler
 
 function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]}
+  return list[Math.floor(Math.random() * list.length)];
+}

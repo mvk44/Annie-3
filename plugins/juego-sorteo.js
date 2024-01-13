51c1af80b9d5ebc938e8199123bf8bd9c73d938f
4nni33 <108834497+4nni33@users.noreply.github.com>
2024-01-13 02:15:35 -0300

    Update juego-sorteo.js

--- plugins/juego-sorteo.js
+++ plugins/juego-sorteo.js
@@ -2,20 +2,19 @@ import util from 'util'
 import path from 'path'
 
 async function handler(m, { groupMetadata, command, conn, text, usedPrefix }) {
-  let user = a => '@' + a.split('@')[0]
-  if (!text) throw `*⚠️ EJEMPLO DE USO:*\n${usedPrefix + command} nombre1 nombre2 nombre3`
+  if (!text) throw `*⚠️ EJEMPLO DE USO:*\n${usedPrefix + command} @usuario1 @usuario2 @usuario3`
   
-  // Obtener una lista de nombres o números proporcionados por el usuario
-  let entries = text.trim().split(' ');
+  // Obtener una lista de participantes mencionados en el comando
+  let entries = text.match(/@(\d{5,}|[a-zA-Z0-9_-]{5,})/g) || [];
 
-  // Si no se proporciona al menos un nombre o número, mostrar un mensaje de error
-  if (entries.length === 0) throw 'Debes proporcionar al menos un nombre o número para el sorteo.';
+  // Si no se menciona a al menos un usuario, mostrar un mensaje de error
+  if (entries.length === 0) throw 'Debes mencionar al menos a un usuario para el sorteo.';
 
   let winner = pickRandom(entries); // Elegir un ganador al azar de la lista
 
   let k = Math.floor(Math.random() * 70)
   let vn = `https://hansxd.nasihosting.com/sound/sound${k}.mp3`
-  let top = `*😼 ${user(winner)} Acaba de ganar el sorteo felicitaciones 🎉*`
+  let top = `*😼 ${winner} Acaba de ganar el sorteo felicitaciones 🎉*`
   let txt = ''
   let count = 0
 
